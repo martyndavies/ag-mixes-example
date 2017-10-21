fbf9dd081a6b6e7ffcac5d618c5703c6c4173972
Martyn Davies <martynrdavies@gmail.com>
2017-10-21 22:52:02 +0200

    update tracklist display

--- js/scripts.js
+++ js/scripts.js
@@ -98,7 +98,12 @@ search.on('render', function () {
         if (content.audio_player != null) {
           console.log(content.objectID + ": " + content.audio_player.toString());
           $('.audio_player').empty();
-            $('.audio_player').append(content.audio_player.toString());
+          //$('.tracklist').slideUp('fast').empty();
+          $('.audio_player').html(content.audio_player.toString());
+          $('.tracklist').slideUp('slow', function(){
+            $('.tracklist').empty();
+            $('.tracklist').html('<ol>'+content.tracklist+'</ol>').slideDown('3000');
+          });
         } else {
           $('.notice_'+content.objectID).fadeIn('fast').delay(3000).fadeOut('slow');
         }
